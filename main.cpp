72e9fd3e4dca78c95b2f842d805bae69aa68b1fb
iraklis07 <h.premptis@gmail.com>
2020-03-19 23:55:59 +0000

    refined

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@ class stack {
 public: 
   stack (int size) {
     data=new T[size];
-    for(int i=0;i<size;i++) {
-      data[i]=0;    
-    }
     top=0;
   }
   stack (const stack &s) { 
@@ -20,6 +17,7 @@ public:
   }
   ~stack () {
     delete [] data;
+    top=0;
   }
   const stack & operator = (const stack &s) {  
     for(int i=0;i<(s.top);i++) {
@@ -58,3 +56,30 @@ private:
   int top;
 }; 
 
+#ifndef CONTEST 
+int main() { 
+  stack<int> s(10);
+  cout << s <<endl;
+  s.push(42);
+  cout << "s has one element: " << s << endl;
+  s.push(17);
+  s.push(34);
+  cout <<"s has more elements: " << s << endl;
+  cout << "How many? " << s.size() << endl;
+  stack<int> t(5);
+  t.push(7);
+  cout<< "t: "<<t<<endl;
+  t=s;
+  cout << "popping from s: " << s.pop() <<endl;
+  s.push(8);
+  stack<int> a(s);
+  t.push(99);
+  a.push(77);
+  cout<<"s: "<<s<<endl;
+  cout<<"t: "<<t<<endl;
+  cout<<"a: "<<a<<endl;
+  stack<char> k(4);
+  k.push('$');
+  cout <<"k contains a character " << k << endl;
+}
+#endif
