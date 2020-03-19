3b90dacf088d3c3816fa6765f9e9f2df62bdd7b5
iraklis07 <h.premptis@gmail.com>
2020-03-19 23:04:29 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class stack {
+public: 
+  stack (int size) {
+    data=new T[size];
+    for(int i=0;i<size;i++) {
+      data[i]=0;    
+    }
+    top=0;
+  }
+  stack (const stack &s) { 
+    data=new T[s.top];
+    for(int i=0;i<s.top;i++) {
+      data[i]=s.data[i];
+      top=s.top;
+    }
+  }
+  ~stack () {
+    delete [] data;
+  }
+  const stack & operator = (const stack &s) {  
+    for(int i=0;i<top;i++) {
+      data[i]=s.data[i];
+    }
+  }
+
+  bool empty () {
+    return top==0;
+  }
+  void push (const T &x) {
+    data[top++]=x;
+  }
+  T pop () {
+    return data[--top];
+  }
+  int size () {
+    return top;
+  }
+
+  friend ostream & operator << (ostream &out, const stack &s) {
+    out << "[";
+    for (int i=0;i<s.top-1;i++){
+       out << s.data[i] <<", " ; 
+    }
+    if (s.top!=0) out << s.data[s.top-1] << "]";
+    else out << "]";
+    return out; 
+  }
+
+private:  
+  T *data;
+  int top;
+}; 
+
+#ifndef CONTEST 
+int main() { 
+  stack<int> s(10);
+  cout << s <<endl;
+  s.push(42);
+  cout << "s has one element: " << s << endl;
+  s.push(17);
+  s.push(34);
+  cout <<"s has more elements: " << s << endl;
+  cout << "How many? " << s.size() << endl;
+  }
+#endif
