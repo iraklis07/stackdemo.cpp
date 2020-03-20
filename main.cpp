8933f78bc5e91d359a04f5bd27aadc4656566130
iraklis07 <h.premptis@gmail.com>
2020-03-20 19:22:57 +0000

    no int main again

--- main.cpp
+++ main.cpp
@@ -9,23 +9,22 @@ public:
     top=0;
   }
   stack (const stack &s) { 
-    data=new T[s.top];
+    data=new T[s.top+1];
     for(int i=0;i<s.top;i++) {
       data[i]=s.data[i];
-      top=s.top;
     }
+    top=s.top;
   }
   ~stack () {
     delete [] data;
-    top=0;
+    data=NULL;
   }
   const stack & operator = (const stack &s) {  
-    for(int i=0;i<(s.top);i++) {
+    for(int i=0;i<s.top;i++) {
       data[i]=s.data[i];
     }
     top=s.top;
     return *this;
-
   }
 
   bool empty () {
@@ -44,7 +43,7 @@ public:
   friend ostream & operator << (ostream &out, const stack &s) {
     out << "[";
     for (int i=0;i<s.top-1;i++){
-       out << s.data[i] <<", " ; 
+       out << s.data[i] <<", "; 
     }
     if (s.top!=0) out << s.data[s.top-1] << "]";
     else out << "]";
@@ -56,30 +55,3 @@ private:
   int top;
 }; 
 
-#ifndef CONTEST 
-int main() { 
-  stack<int> s(10);
-  cout << s <<endl;
-  s.push(42);
-  cout << "s has one element: " << s << endl;
-  s.push(17);
-  s.push(34);
-  cout <<"s has more elements: " << s << endl;
-  cout << "How many? " << s.size() << endl;
-  stack<int> t(5);
-  t.push(7);
-  cout<< "t: "<<t<<endl;
-  t=s;
-  cout << "popping from s: " << s.pop() <<endl;
-  s.push(8);
-  stack<int> a(s);
-  t.push(99);
-  a.push(77);
-  cout<<"s: "<<s<<endl;
-  cout<<"t: "<<t<<endl;
-  cout<<"a: "<<a<<endl;
-  stack<char> k(4);
-  k.push('$');
-  cout <<"k contains a character " << k << endl;
-}
-#endif
